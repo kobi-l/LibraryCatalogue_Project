0864e9d9148f695c1c28b3663a3323cc4b21a9a6
Mr.White <kobilabok@users.noreply.github.com>
2019-11-22 14:52:16 -0600

    AssemblyTitle update

--- LibraryCatalog/Properties/AssemblyInfo.cs
+++ LibraryCatalog/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("LibraryCatalog")]
+[assembly: AssemblyTitle("LibraryCatalogue")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("LibraryCatalog")]
+[assembly: AssemblyProduct("LibraryCatalogue")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
